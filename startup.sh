859906bd24796d5f6337d8a833d60d41af90f5a3
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-01-31 14:56:41 +1300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 echo "Starting LogonTracer..."
 
+sleep 20s
 
 cd /home/kasm-user/LogonTracer/ && python3 logontracer.py -r -o 8080 -u neo4j -p neo4j -s localhost &
 
 sleep 10s
+
+echo "Starting Firefox..."
+
 
 firefox --new-window http://localhost:8080
 
