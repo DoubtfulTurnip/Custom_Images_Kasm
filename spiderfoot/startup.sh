b435010c008fb465e0bd15afdb79927fcb020a1b
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-02-07 07:46:10 +1300

    Update run cmd

--- spiderfoot/startup.sh
+++ spiderfoot/startup.sh
@@ -3,11 +3,11 @@ echo "Starting Spiderfoot..."
 
 sleep 20s
 
-cd /home/kasm-user/spiderfoot/ && python3 sf.py &
+cd /home/kasm-user/spiderfoot/ && python3 sf.py -l 127.0.0.1:5001 &
 
 sleep 10s
 
 echo "Starting Firefox..."
 
 
-firefox --new-window http://localhost:8080
+firefox --new-window http://localhost:5001
