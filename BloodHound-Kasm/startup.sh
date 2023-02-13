cbd0478e41ded7bd3f177e14899dc1fc80405c4c
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-02-14 11:18:13 +1300

    Fixing typo in startup.sh and issue with collector

--- BloodHound-Kasm/startup.sh
+++ BloodHound-Kasm/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "Starting LogonTracer..."
+echo "Starting BloodHound..."
 
 sleep 20s
 
-cd /home/kasm-user/bloodhound-Kasm/ && ./bloudhound.bin --no-sandbox &
+cd /home/kasm-user/bloodhound-kasm/ && ./bloudhound.bin --no-sandbox &
 
 sleep 10s
 
