210f7e0f61b136ea9f4df8276acbbb23b96577c0
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-02-14 08:25:47 +1300

    Updates to folder paths

--- Spiderfoot-Kasm/startup.sh
+++ Spiderfoot-Kasm/startup.sh
@@ -3,7 +3,7 @@ echo "Starting Spiderfoot..."
 
 sleep 20s
 
-cd /home/kasm-user/spiderfoot/ && python3 sf.py -l 127.0.0.1:5001 &
+cd /home/kasm-user/Spiderfoot-Kasm/ && python3 sf.py -l 127.0.0.1:5001 &
 
 sleep 10s
 
