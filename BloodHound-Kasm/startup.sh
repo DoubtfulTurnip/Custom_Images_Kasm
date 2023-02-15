9ee208f2ee8537af6a66a2da8b29a13154affbd8
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-02-16 11:47:32 +1300

    Update startup.sh

--- BloodHound-Kasm/startup.sh
+++ BloodHound-Kasm/startup.sh
@@ -2,11 +2,6 @@
 echo "Starting BloodHound..."
 
 #Download BH collectors
-wget https://github.com/BloodHoundAD/AzureHound/releases/latest/download/azurehound-linux-amd64.zip \
-    && unzip azurehound-linux-amd64.zip \
-    && rm azurehound-linux-amd64.zip \
-    && mv azurehound /usr/bin
-
 git clone https://github.com/BloodHoundAD/BloodHound.git $HOME/Desktop/BLOODHOUND
 mv $HOME/Desktop/BLOODHOUND/Collectors $HOME/Desktop/ 
 rm $HOME/Desktop/BLOODHOUND/Collectors/AzureHound.md
