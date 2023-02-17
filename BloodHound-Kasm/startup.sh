1f1c1422c6088d68408f78b83f10784e86ee54d6
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-02-17 17:37:47 +1300

    fix permissions and changing collector script

--- BloodHound-Kasm/startup.sh
+++ BloodHound-Kasm/startup.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 echo "Starting BloodHound..."
-
-echo "Acquiring Collectors and starting BloodHound..."
-git clone https://github.com/BloodHoundAD/BloodHound.git $HOME/Desktop/BLOODHOUND \
-&& mv $HOME/Desktop/BLOODHOUND/Collectors $HOME/Desktop/Bloodhound-Collectors \
-&& rm $HOME/Desktop/Collectors/AzureHound.md \
-&& rm -rf $HOME/Desktop/BLOODHOUND \
-&& sleep 5s \
+sleep 60s \
 && /opt/BloodHound-linux-x64/BloodHound --no-sandbox
 
 
