d6d5bba40c7938ebf84e34f6cd888e4065e3cdad
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-02-17 17:23:08 +1300

    Fixing install

--- BloodHound-Kasm/startup.sh
+++ BloodHound-Kasm/startup.sh
@@ -3,11 +3,11 @@ echo "Starting BloodHound..."
 
 echo "Acquiring Collectors and starting BloodHound..."
 git clone https://github.com/BloodHoundAD/BloodHound.git $HOME/Desktop/BLOODHOUND \
-&& mv $HOME/Desktop/BLOODHOUND/Collectors $HOME/Desktop/ \
+&& mv $HOME/Desktop/BLOODHOUND/Collectors $HOME/Desktop/Bloodhound-Collectors \
 && rm $HOME/Desktop/Collectors/AzureHound.md \
 && rm -rf $HOME/Desktop/BLOODHOUND \
 && sleep 5s \
-&& BloodHound --no-sandbox 
+&& /opt/BloodHound-linux-x64/BloodHound --no-sandbox
 
 
 
