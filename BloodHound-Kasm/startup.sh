340a554e69b2ef3bbcbfa9b18a01817298a13e2a
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2023-02-17 18:54:11 +1300

    remove collectors

--- BloodHound-Kasm/startup.sh
+++ BloodHound-Kasm/startup.sh
@@ -3,10 +3,7 @@ echo "Starting BloodHound..."
 sleep 60s \
 && /opt/BloodHound-linux-x64/BloodHound --no-sandbox &
 
-git clone https://github.com/BloodHoundAD/BloodHound.git $HOME/Desktop/BLOODHOUND \
-&& mv $HOME/Desktop/BLOODHOUND/Collectors $HOME/Desktop/Bloodhound-Collectors \
-&& rm $HOME/Desktop/Collectors/AzureHound.md \
-&& rm -rf $HOME/Desktop/BLOODHOUND 
+
 
 
 
