9dbc375da26058394d7e0b71b5c86619d32f538f
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2025-04-09 16:42:40 +1200

    Refactor custom startup script for Webcheck to improve command execution flow

--- Webcheck-Kasm/custom_startup_webcheck.sh
+++ Webcheck-Kasm/custom_startup_webcheck.sh
@@ -3,14 +3,16 @@
 
 # Wait for the Kasm desktop environment to be fully ready.
 # (This is needed so that your startup commands run after the environment loads.)
-/usr/bin/desktop_ready && \
+/usr/bin/desktop_ready
+
+# Change to the webcheck directory.
+cd /web-check
 
 # Start the webcheck development server in the background.
-# Make sure the repository is cloned at /web-check.
-cd /web-check && yarn dev \
+yarn dev &
 
 # Wait a few seconds to allow the dev server to initialize.
-sleep 5 && \
+sleep 5
 
 # Launch Firefox in kiosk mode to display the webcheck UI.
 firefox --kiosk http://localhost:4321 &
