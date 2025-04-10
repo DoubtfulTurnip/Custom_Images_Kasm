f32265da7a7ba7765d520fd7b4be343ef73191fd
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2025-04-10 13:32:34 +1200

    Add Dockerfile and custom startup script for LogonTracer deployment

---
+++ LogonTracer-Kasm/custom_startup_logontracer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Custom startup script for BloodHound CE in a full desktop environment using Google Chrome.
+
+sudo service docker start
+
+# Display a desktop notification to inform the user.
+notify-send -t 60000 "LogonTracer is starting" "Please wait while LogonTracer services are being deployed."
+
+
+docker container run \
+   --detach \
+   --publish=7474:7474 --publish=7687:7687 --publish=8080:8080 \
+   -e LTHOSTNAME=127.0.0.1 \
+   jpcertcc/docker-logontracer
+
+
+sleep 60
+
+notify-send -t 30000 "LogonTracer has started" 
+
+google-chrome --start-maximized http://127.0.0.1:8080 &
