edec93527ea19f687c89f328a6a86aee69506dd2
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2025-04-10 13:51:16 +1200

    Refactor custom startup script for LogonTracer: streamline docker run command and adjust sleep timing

--- LogonTracer-Kasm/custom_startup_logontracer.sh
+++ LogonTracer-Kasm/custom_startup_logontracer.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 # Custom startup script for BloodHound CE in a full desktop environment using Google Chrome.
 
+sleep 5 
+
 sudo service docker start
 
 # Display a desktop notification to inform the user.
 notify-send -t 60000 "LogonTracer is starting" "Please wait while LogonTracer services are being deployed."
 
-
-docker container run \
-   --detach \
-   --publish=7474:7474 --publish=7687:7687 --publish=8080:8080 \
-   -e LTHOSTNAME=127.0.0.1 \
-   jpcertcc/docker-logontracer
-
+docker container run --detach --publish=7474:7474 --publish=7687:7687 --publish=8080:8080 -e LTHOSTNAME=127.0.0.1 jpcertcc/docker-logontracer
 
 sleep 60
 
